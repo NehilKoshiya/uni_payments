ae72b481fd7d27d3cdf4bc87f34cf2b39bef5218
Nehil Koshiya <nehil_koshiya@idealake.com>
2026-07-21 12:27:25 +0530

    Release uni_payments 0.0.7

--- example/lib/ui/sliver_header.dart
+++ example/lib/ui/sliver_header.dart
@@ -98,7 +98,7 @@ class _VersionPill extends StatelessWidget {
         border: Border.all(color: Colors.white.withValues(alpha: 0.12)),
       ),
       child: Text(
-        'v0.0.6',
+        'v0.0.7',
         style: textTheme.labelSmall?.copyWith(
           color: Colors.white.withValues(alpha: 0.75),
           fontFeatures: const [FontFeature.tabularFigures()],
